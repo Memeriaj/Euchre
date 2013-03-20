e9376d451eace824c8c7c711c490c48058f9535a
Erik Sanders <sanderej@rose-hulman.edu>
2013-03-19 22:05:26 -0400

    added some basic card code and a few classes we will need

---
+++ src/Card.java
@@ -0,0 +1,20 @@
+
+public class Card 
+{
+	public enum SUIT
+	{
+		CLUBS,
+		DIAMONDS,
+		HEARTS,
+		SPADES
+	}
+	
+	public final SUIT suit;
+	public final int value;
+	
+	public Card(SUIT s, int val)
+	{
+		this.suit = s;
+		this.value = val;
+	}
+}
