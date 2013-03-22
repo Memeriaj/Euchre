cb5567baa2a507163d1509634aaf512e8185b84d
hallowlt <hallowlt@rose-hulman.edu>
2013-03-21 20:35:19 -0400

    EuchreTest

--- src/EuchreTest.java
+++ src/EuchreTest.java
@@ -7,8 +7,34 @@ import java.util.LinkedList;
 public class EuchreTest {
 	
 	@Test
-	public void test(){
+	public void initialize(){
 		Euchre e = new Euchre();
+		assert(true);
+	}
+	
+	@Test
+	public void deckNotNull(){
+		Euchre e = new Euchre();
+		assertFalse(e.deck == new LinkedList<Card>());
+	}
+	
+	@Test
+	public void playerkNotNull(){
+		Euchre e = new Euchre();
+		assertFalse(e.players == new ArrayList<Player>());
+	}
+	
+	@Test
+	public void allCardsNotNull(){
+		Euchre e = new Euchre();
+		assertFalse(e.allCards == new ArrayList<Card>());
+	}
+	
+	@Test
+	public void deckRandom(){
+		Euchre e = new Euchre();
+		for(int x=0; x<5; x++)
+			System.out.println(e.draw().toString());
 		assert(true);
 	}
 }
