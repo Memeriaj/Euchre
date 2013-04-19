0771df03c0162b1b60e4ab6149a55e0ee9266348
memeriaj <memeriaj@rose-hulman.edu>
2013-04-18 22:23:48 -0400

    Good deal of GUI operations for preround setup done

--- src/Controller.java
+++ src/Controller.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 
 
 public class Controller {
@@ -14,29 +16,75 @@ public class Controller {
 	 * @throws InterruptedException 
 	 */
 	public void cardPlayed(String cardText) {
+		if(euchre.currentRound.isInPreGameState && euchre.currentRound.dealer == 0){
+			
+		}
 		euchre.humanPlayCard(cardText);
 		while (euchre.isCurrentPlayerAI())
 		{
 			euchre.makeAIPlay();
 		}
 		updateGUI();
+		if(euchre.currentRound.isInPreGameState){
+			setUpIntitalRound();
+		}
 	}
 	
+	private void setUpIntitalRound() {
+		boolean[] disabled = {false,false,false,false};
+		applicationWindow.setPlayersCardsEnabled(disabled);
+		String[] buttonsText = {"Pass", "Pick it up"};
+		applicationWindow.setExtraButtonDisplay(buttonsText);
+		applicationWindow.setMiddleTextArea("Card Turned up: "+euchre.currentRound.turnedUpCard.toString());
+		applicationWindow.refreshWindow();
+	}
+
 	private void updateGUI()
 	{
 		String text = euchre.currentRound.produceTrickHistoryText();
 		
 		applicationWindow.updateHands(euchre.getAllHands());
 		applicationWindow.setMiddleTextArea(Utils.convertStringToHTML(text));
+		String trump = "";
+		if(euchre.currentRound.trump == null)
+			trump = "Trump hasn't been set yet";
+		else
+			trump = euchre.currentRound.trump.toString();
 		applicationWindow.setScoreDisplay(euchre.score[0], euchre.score[1], euchre.currentRound.trickCount[0],
-				euchre.currentRound.trickCount[1], euchre.currentRound.trump.toString());
-		applicationWindow.setPlayersCardsEnabled(euchre.getPlayableCardsForHuman());
+				euchre.currentRound.trickCount[1], trump);
+		boolean[] cardsEnabled = {false, false, false, false, false};
+		if(!euchre.currentRound.isInPreGameState)
+			cardsEnabled = euchre.getPlayableCardsForHuman();
+		applicationWindow.setPlayersCardsEnabled(cardsEnabled);
 		applicationWindow.refreshWindow();
 	}
 
 	public void setAppWindow(ApplicationWindow appWin) {
 		applicationWindow = appWin;
 		updateGUI();
+		setUpIntitalRound();
+	}
+
+	public void extraButtonSelected(String text) {
+		if(euchre.currentRound.isCardTurnedUp){
+			if(text == "Pass")
+				euchre.currentRound.preRoundPass();
+			else if(euchre.currentRound.dealer == 0)
+				setUpDiscard();
+			else
+				euchre.currentRound.preRoundCall();
+		}
+	}
+
+	private void setUpDiscard() {
+		updateGUI();
+		ArrayList<ArrayList<Card>> hands = euchre.getAllHands();
+		hands.get(0).add(euchre.currentRound.turnedUpCard);
+		applicationWindow.updateHands(hands);
+		boolean[] enabled = {true, true, true, true, true, true};
+		applicationWindow.setPlayersCardsEnabled(enabled);
+		applicationWindow.setMiddleTextArea("Please select a card to discard.");
+		applicationWindow.refreshWindow();
 	}
 
 }
